4a7dbc4818f0151e6aa71cf5795435d6066b2d4d
Fanucchi <ffanucchi@studiovisual.com.br>
2019-08-07 14:15:57 -0300

    Salvando usuario liked dentro do db

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -1,7 +1,23 @@
 const Dev = require('../models/Dev');
 
 module.exports = {
-    store(request, response) {
-        return response.json({ ok: true })
+    async store(request, response) {
+        const { devId } = request.params;
+        const { user } = request.headers;
+
+        const loggedDev = await Dev.findById(user);
+        const targetDev = await Dev.findById(devId);
+
+        if (!targetDev) {
+            return response
+                .status(400)
+                .json({ error: 'Dev not exists' });
+        }
+
+        loggedDev.likes.push(targetDev._id);
+
+        await loggedDev.save();
+
+        return response.json(loggedDev);
     }
 };
