353506e3fdda9070e12fa56e5dcdfacfe574d02f
Fanucchi <ffanucchi@studiovisual.com.br>
2019-08-10 15:03:49 -0300

    Adiciona funcionalidade de like e dislike.

--- frontend/src/pages/Main/Main.js
+++ frontend/src/pages/Main/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
@@ -20,36 +21,54 @@ export default function Main({ match }) {
         })();
     }, [match.params.id]);
 
+    async function handleLike(id) {
+        const response = await api.post(`/devs/${id}/like`, null, {
+            headers: { user: match.params.id }
+        });
+        setUsers(users.filter(user => user._id !== id));
+    }
+
+
+    async function handleDislike(id) {
+        await api.post(`/devs/${id}/dislike`, null, {
+            headers: { user: match.params.id }
+        });
+        setUsers(users.filter(user => user._id !== id));
+    }
+
     return(
         <div className="main-container">
-            <img className="logo" src={logo} alt="Tindev"/>
-            <ul className="list-dev">
-                {users.map(user => (
-                    <li className="dev">
-                        <figure>
-                            <img className="dev-avatar" src={user.avatar} alt={user.name} />
-                            <div className="dev-info">
-                                <footer className="dev-footer">
-                                    <strong>{user.name}</strong>
-                                    <p className="bio">
-                                        {user.bio}
-                                    </p>
-                                </footer>
+            <Link to="/">
+                <img className="logo" src={logo} alt="Tindev"/>
+            </Link>
+            {users.length === 0 ? <h1 className="empty">Acabou, volte mais tarde!</h1> : (
+                <ul className="list-dev">
+                    {users.map(user => (
+                        <li className="dev">
+                            <figure>
+                                <img className="dev-avatar" src={user.avatar} alt={user.name} />
+                                <div className="dev-info">
+                                    <footer className="dev-footer">
+                                        <strong>{user.name}</strong>
+                                        <p className="bio">
+                                            {user.bio}
+                                        </p>
+                                    </footer>
+                                </div>
+                            </figure>
+                            
+                            <div className="buttons">
+                                <button type="button" onClick={() => handleDislike(user._id)}>
+                                    <img src={dislike} alt="dislike"/>
+                                </button>
+                                <button type="button" onClick={() => handleLike(user._id)}>
+                                    <img src={like} alt="Like" />
+                                </button>
                             </div>
-                        </figure>
-                        
-                        <div className="buttons">
-                            <button type="button">
-                                <img src={dislike} alt="dislike"/>
-                            </button>
-                            <button type="button">
-                                <img src={like} alt="Like" />
-                            </button>
-                        </div>
-                    </li>
-
-                ))}
-            </ul>
+                        </li>
+                    ))}
+                </ul>
+            )}
         </div>
     )
 };
